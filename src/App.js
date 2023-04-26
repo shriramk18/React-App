89e4f5f238c3c9398675d4fce18e198b047b8e3b
shriramk18 <Shriramkakade2000@gmail.com>
2023-04-26 14:47:55 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,139 +1,137 @@
-// import logo from './logo.svg';
-import Fragment from 'react'
 import './App.css';
-import { ProductCard } from './components/ProductCard/ProductCard';
+import { Productcard } from './components/ProductCard/ProductCard';
+import { Fragment } from 'react';
+
 function App() {
- 
-     const product1 = {
-      productPic: "https://tse4.mm.bing.net/th?id=OIP.5DkTfmZhmkb34y6tzbb4fAHaE_&pid=Api&P=0",
-      productId: "1234",
-      productName: " Air Jorden",
-      Brand: "Nike",
-      productDescription: "This shoe is Air Jorden very much trending.",
-      Price: "$65",
-      Rating: "4.5"
-  
-    }
-  
-    const product2 = {
-      productPic: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkBq0v4aYt67mjjPO8f-8pSd4Zhn2tzgWbqqHZhV-w&s",
-      productId: "1234",
-      productName: " Johnson ",
-      Brand: "Nike",
-      productDescription: "This Johnson watch is very  much trending",
-      Price: "$85",
-      Rating: "4.5"
-    }
-  
-    const product3 = {
-      productPic: "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aGVhZHBob25lc3xlbnwwfHwwfHw%3D&w=1000&q=80",
-      productId: "1234",
-      productName: "boAt Headphones",
-      Brand: "Nike",
-      productDescription: "This Headphones is very much   trending.",
-      Price: "$65",
-      Rating: "4.5"
-    }
-  
-    const product4 = {
-  
-      productPic: "https://cdn.shopify.com/s/files/1/0490/6011/8686/products/CVC-Sunscreen.jpg?v=1665755509",
-      productId: "1234",
-      productName: " Sunscreen",
-      Brand: "Nike",
-      productDescription: "This Sunscreen is very much trending.",
-      Price: "$65",
-      Rating: "4.5"
-    }
-  
-    const product5 = {
-      productPic: "https://tse2.mm.bing.net/th?id=OIP.m26yupS4vCSbyhAo32Ni2gHaFN&pid=Api&P=0",
-      productId: "1234",
-      productName: " Nike ",
-      Brand: "Nike",
-      productDescription: "This nike shoe is very much trending.",
-      Price: "$65",
-      Rating: "4.5"
-    }
-  
-  
-    function productalert1() {
-      alert("product details "+ product1.productName)
-    }
-    function productalert2() {
-      alert("product details "+ product2.productName)
-    }
-    function productalert3() {
-      alert("product details "+ product3.productName)
-    }
-    function productalert4() {
-      alert("product details "+ product4.productName)
-    }
-    function productalert5() {
-      alert("product details "+ product5.productName)
-    }
-  
-    const card1 = {
-      background: 'orange'
-    }
-  
-  
-    const card2 = {
-      background: 'blue'
-    }
-  
-  
-    const card3 = {
-      background: 'green'
-    }
-  
-  
-    const card4 = {
-      background: 'red'
-    }
-  
-    const card5 = {
-      background: 'yellow'
-    }
-  
-    return (
-  
-      <Fragment>
-        <div className='child_container'>
-  
-          <ProductCard
-            productDetails={product1}
-            alertfun={productalert1}
-            button={card1}
-  
-          />
-          <ProductCard
-            productDetails={product2}
-            alertfun={productalert2}
-            button={card2}
-  
-          />
-          <ProductCard
-            productDetails={product3}
-            alertfun={productalert3}
-            button={card3}
-          />
-          <ProductCard
-            productDetails={product4}
-            alertfun={productalert4}
-            button={card4}
-          />
-          <ProductCard
-            productDetails={product5}
-            alertfun={productalert5}
-            button={card5}
-          />
-        </div>
-      </Fragment>
-    );
-  
-  
+
+
+  const product1 = {
+    productPic: "https://images.pexels.com/photos/786003/pexels-photo-786003.jpeg?auto=compress&cs=tinysrgb&w=400",
+    id: "1234",
+    productName: "Nike",
+    Brand: "Nike",
+    productDescription: "This is best shoe to use",
+    Price: "$65",
+    Rating: "4.5"
+
+  }
+
+  const product2 = {
+    productPic: "https://images.pexels.com/photos/2783873/pexels-photo-2783873.jpeg?auto=compress&cs=tinysrgb&w=400",
+    id: "1234",
+    productName: "Watch",
+    Brand: "Nike",
+    productDescription: "This watch is best ",
+    Price: "$85",
+    Rating: "4.5"
+  }
+
+  const product3 = {
+    productPic: "https://images.pexels.com/photos/581087/pexels-photo-581087.jpeg?auto=compress&cs=tinysrgb&w=400",
+    id: "1234",
+    productName: "T-Shirts",
+    Brand: "Nike",
+    productDescription: "Some branded t-shirts  ",
+    Price: "$65",
+    Rating: "4.5"
+  }
+
+  const product4 = {
+
+    productPic: "https://cdn.shopify.com/s/files/1/0490/6011/8686/products/CVC-Sunscreen.jpg?v=1665755509",
+    id: "1234",
+    productName: " Sunscreen",
+    Brand: "Nike",
+    productDescription: "This Sunscreen is best",
+    Price: "$65",
+    Rating: "4.5"
+  }
+
+  const product5 = {
+    productPic: "https://images.pexels.com/photos/2759783/pexels-photo-2759783.jpeg?auto=compress&cs=tinysrgb&w=400",
+    id: "1234",
+    productName: " PUMA ",
+    Brand: "Nike",
+    productDescription: "This PUMA shoe is branded",
+    Price: "$65",
+    Rating: "4.5"
+  }
+
+
+  function productalert1() {
+    alert("product details "+ product1.productName)
+  }
+  function productalert2() {
+    alert("product details "+ product2.productName)
+  }
+  function productalert3() {
+    alert("product details "+ product3.productName)
+  }
+  function productalert4() {
+    alert("product details "+ product4.productName)
+  }
+  function productalert5() {
+    alert("product details "+ product5.productName)
+  }
+
+  const card1 = {
+    background: 'orange'
+  }
+
+
+  const card2 = {
+    background: 'blue'
+  }
+
+
+  const card3 = {
+    background: 'green'
+  }
+
+
+  const card4 = {
+    background: 'red'
+  }
+
+  const card5 = {
+    background: 'yellow'
+  }
+
+  return (
+
+    <Fragment>
+      <div className='child_container'>
+
+        <Productcard
+          productDetails={product1}
+          alertfun={productalert1}
+          button={card1}
+
+        />
+        <Productcard
+          productDetails={product2}
+          alertfun={productalert2}
+          button={card2}
+
+        />
+        <Productcard
+          productDetails={product3}
+          alertfun={productalert3}
+          button={card3}
+        />
+        <Productcard
+          productDetails={product4}
+          alertfun={productalert4}
+          button={card4}
+        />
+        <Productcard
+          productDetails={product5}
+          alertfun={productalert5}
+          button={card5}
+        />
+      </div>
+    </Fragment>
+  );
 }
 
 export default App;
- 
--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,18 +1,17 @@
-// import { Fragment } from 'react'
 import './ProductCard.css'
 
-export function ProductCard({ productDetails, alertfun, button }) {
+export function Productcard({ productDetails, alertfun, button }) {
 
 
     const
-        { productId, productPic, productName, Brand, productDescription, Price, Rating, } = productDetails
+ { productId, productPic, productName, Brand, productDescription, Price, Rating, } = productDetails
 
 
     return (
 
         <div className='card'>
             <div className='card2'>
-                <img src={productPic} alt='' />
+                <img src={productPic} />
                 <h3 className="name">{productName}</h3>
                 <p className="description">{productDescription}</p>
                 <h3 className="price">{Price}</h3>
