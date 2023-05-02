1ee2c910e0af8ec1eb64abcfba3bbc207da239be
shriramk18 <Shriramkakade2000@gmail.com>
2023-05-02 11:11:02 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Ur from './components/Ur/Ur';
+import Ueff from './components/Ueff/Ueff';
 
 function App() {
   return (
     <div className="App">
      <Ur/>
+     {/* <Ueff/> */}
     </div>
   );
 }
---
+++ src/components/Ueff/Ueff.js
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react'
+
+export default function Ueff() {
+
+    const img=useEffect(null)
+
+    function handleClick(){
+
+    }
+
+  return (
+    <div>
+        <img src='pexels-lina-kivaka-4482677.jpg' alt=''/>
+        <button>Click to change</button>
+    </div>
+  )
+}
